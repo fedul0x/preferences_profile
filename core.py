d526c6249f1868dc70d674005f74eb3c14e545e1
Alexey Ivashin <ivashin.alexei@gmail.com>
2013-01-14 20:59:20 +0400

    correct some database saving process errors

--- core.py
+++ core.py
@@ -94,6 +94,41 @@ def main():
 
 	cursor.execute("SELECT id, state FROM profiles_type WHERE dimension_n = %s and dimension_m = %s", (n, m))
 	res = cursor.fetchone()
+	print res
+	if cursor.rowcount != 1:
+		if cursor.rowcount > 1:
+			cursor.execute("DELETE FROM profiles_type WHERE dimension_n = %s and dimension_m = %s",	(n, m))
+			connection.commit()
+		print 'insert processing'
+		cursor.execute("""INSERT INTO profiles_type (dimension_n, dimension_m, state)
+			VALUES (%s, %s, %s)""", (n, m, 'processing'))
+		cursor.execute("SELECT id, state FROM profiles_type WHERE dimension_n = %s and dimension_m = %s", (n, m))
+		res = cursor.fetchone()
+		connection.commit()
+		all_comb = []
+		for x in create_decomposition_into_components(n):
+			l = interpretate_decomposition_to_sum_list(x, m)
+			for p in permutations(l):
+				if not (p in all_comb):
+					all_comb.append(p)
+					try:
+					# print res[0], [o for o in p]
+						cursor.execute("""
+							INSERT INTO combination_of_alternative_distribution (profiles_type_id, combination) 
+							VALUES (%s, %s)""", (res[0], [o for o in p]))
+						print "Added %s" % (p, )
+						connection.commit()
+					except:
+						print "Error %s" % (p, )
+						connection.commit()
+		cursor.execute("SELECT id, state FROM profiles_type WHERE dimension_n = %s and dimension_m = %s", (n, m))
+		res = cursor.fetchone()
+		cursor.execute("""UPDATE profiles_type SET state = 'filling' WHERE id = %s; """, (res[0], ))
+		connection.commit()
+
+	cursor.execute("SELECT id, state FROM profiles_type WHERE dimension_n = %s and dimension_m = %s", (n, m))
+	res = cursor.fetchone()
+	print res
 	if cursor.rowcount == 1:
 		if res[1] == 'processing':
 			all_comb = []
@@ -110,17 +145,16 @@ def main():
 							connection.commit()
 						except:
 							print "Error %s" % (p, )
-							pass
+							connection.commit()
 			cursor.execute("""UPDATE profiles_type SET state = 'filling' WHERE id = %s; """, (res[0], ))
 			connection.commit()
-		elif res[1] == 'ok':
-			print 'This task is ok!'
-			return 0
-		elif res[1] == 'checking':
+		cursor.execute("SELECT id, state FROM profiles_type WHERE dimension_n = %s and dimension_m = %s", (n, m))
+		res = cursor.fetchone()
+		print res
+		if res[1] == 'filling':
 			cursor.execute("""SELECT DISTINCT(combination) FROM combination_of_alternative_distribution 
 				WHERE profiles_type_id = %s""", (res[0],))
 			all_comb = [c[0] for c in cursor.fetchall()]
-		elif res[1] == 'filling':
 			z = len(all_comb)
 			k = 0
 			for c in combinations_with_replacement([i for i in xrange(0, z)], m):
@@ -131,14 +165,34 @@ def main():
 					print "Added %s" % (c, )
 				except:
 					print "Error %s" % (c, )
+					connection.commit()
 			cursor.execute("""UPDATE profiles_type SET state = 'checking' WHERE id = %s; """, (res[0], ))
 			connection.commit()
 			cursor.execute("""SELECT DISTINCT(combination) FROM combination_of_alternative_distribution 
 				WHERE profiles_type_id = %s""", (res[0],))
 			all_comb = [c[0] for c in cursor.fetchall()]
-		else:
-			print 'Unknown status %s' % (res[1], )
+
+		cursor.execute("SELECT id, state FROM profiles_type WHERE dimension_n = %s and dimension_m = %s", (n, m))
+		res = cursor.fetchone()
+		print res
+		if res[1] == 'checking':
+			cursor.execute("""SELECT DISTINCT(combination) FROM combination_of_alternative_distribution 
+				WHERE profiles_type_id = %s""", (res[0],))
+			all_comb = [c[0] for c in cursor.fetchall()]
+		
+		cursor.execute("SELECT id, state FROM profiles_type WHERE dimension_n = %s and dimension_m = %s", (n, m))
+		res = cursor.fetchone()
+		print res
+		if res[1] == 'ok':
+			print 'This task is ok!'
+			cursor.close()
+			connection.close()
 			return 0
+	# 	else:
+	# 		print 'Unknown status %s' % (res[1], )
+	# 		return 0
+	# else:
+
 
 	# currentLimit = 0
 	# currentOffset = 1
@@ -146,8 +200,8 @@ def main():
 	# currentLimit = currentLimit + stepLimit
 	# TODO add change status to process
 	cursor.execute("""SELECT id, combination_of_combination FROM combination_of_combination_of_alternative_distribution 
-		WHERE profiles_type_id = %s and state = %s ORDER BY id LIMIT %s """, (res[0], 'uncheck', currentLimit))
-	all_comb_of_comb = cur.fetchall()
+		WHERE profiles_type_id = %s and state = %s ORDER BY id LIMIT %s """, (res[0], 'uncheck', stepLimit))
+	all_comb_of_comb = cursor.fetchall()
 	zz = len(all_comb_of_comb)
 	k = 0
 	while (zz > 0):
@@ -165,17 +219,16 @@ def main():
 		# cur.execute("""SELECT id, combination_of_combination FROM combination_of_combination_of_alternative_distribution 
 			# WHERE dimension_n = %s and dimension_m = %s and state = %s  ORDER BY id LIMIT %s OFFSET %s""", (n, m, 'uncheck', currentLimit, currentOffset))
 		cursor.execute("""SELECT id, combination_of_combination FROM combination_of_combination_of_alternative_distribution 
-			WHERE profiles_type_id = %s and state = %s  ORDER BY id LIMIT %s""", (res[0], 'uncheck', currentLimit))
-		all_comb_of_comb = cur.fetchall()
+			WHERE profiles_type_id = %s and state = %s  ORDER BY id LIMIT %s""", (res[0], 'uncheck', stepLimit))
+		all_comb_of_comb = cursor.fetchall()
 		zz = len(all_comb_of_comb)
 		# print "%s - %s" % (currentOffset, currentLimit)
-
+	cursor.execute("""UPDATE profiles_type SET state = 'ok' WHERE id = %s; """, (res[0], ))
 	print k
 	connection.commit()
-
+	print 'This task is ok!'
 	cursor.close()
 	connection.close()
-
 
 
 if __name__ == '__main__':
